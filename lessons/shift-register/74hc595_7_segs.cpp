47ff5e0dfa6152185aefaf56e15f38dfbb382274
Vinh Thuy Tran <vinhthuy.tran@zuhlke.com>
2024-01-12 11:31:45 +0700

    edit shift register example

--- lessons/shift-register/74hc595_7_segs.cpp
+++ lessons/shift-register/74hc595_7_segs.cpp
@@ -3,11 +3,14 @@
 #include <freertos/task.h>
 #include <freertos/timers.h>
 #include <driver/gpio.h>
+#include "esp_log.h"
 
-#define SRCLK GPIO_NUM_6 // clock
-#define RCLK GPIO_NUM_7 // latch
-#define DS GPIO_NUM_8 // data
-#define BTN GPIO_NUM_2
+#define SHCP GPIO_NUM_16 // clock
+#define STCP GPIO_NUM_17 // latch
+#define DS GPIO_NUM_5 // data
+#define BTN GPIO_NUM_4
+
+static const char *TAG = "shift_register";
 
 // array of 7-segment display digits in pull-down mode
 int digits[10][8] = {
@@ -23,29 +26,42 @@ int digits[10][8] = {
     {0, 1, 1, 1, 1, 0, 1, 1}  // 9
 };
 
-static uint8_t count = -1;
+static uint8_t count = 0;
+
+void setSegment() 
+{
+    gpio_set_level(STCP, 0);
+    for (int i = 7; i >= 0 ; i--) {
+        gpio_set_level(SHCP, 0);
+        gpio_set_level(DS, !digits[count][i]);
+        gpio_set_level(SHCP, 1);
+    }
+    gpio_set_level(STCP, 1);
+}
 
 void task1(void *pvParameter)
 {
     ++count;
 
-    gpio_set_level(RCLK, 0);
-    for (int i = 7; i >= 0 ; i--) {
-        gpio_set_level(DS, digits[count][i]);
-        gpio_set_level(SRCLK, 1);
-        gpio_set_level(SRCLK, 0);
+    if(count > 9) {
+      count = 0;
     }
-    gpio_set_level(RCLK, 1);
+
+    ESP_LOGI(TAG, "count %d/n", count);
+
+    setSegment();
+
     vTaskDelete(NULL);
 }
 
 void setup() {
-    gpio_reset_pin(SRCLK);
-    gpio_reset_pin(RCLK);
-    gpio_reset_pin(DS);
-    
-    gpio_set_direction(SRCLK, GPIO_MODE_OUTPUT);
-    gpio_set_direction(RCLK, GPIO_MODE_OUTPUT);
+    // gpio_reset_pin(STCP);
+    // gpio_reset_pin(SHCP);
+    // gpio_reset_pin(DS);
+    // gpio_reset_pin(BTN);
+
+    gpio_set_direction(SHCP, GPIO_MODE_OUTPUT);
+    gpio_set_direction(STCP, GPIO_MODE_OUTPUT);
     gpio_set_direction(DS, GPIO_MODE_OUTPUT);
     gpio_set_direction(BTN, GPIO_MODE_INPUT);    
     gpio_set_pull_mode(BTN, GPIO_PULLUP_ONLY);
@@ -55,11 +71,18 @@ extern "C" void app_main(void)
 {
     setup();
 
+    setSegment();
+
     while (1)
     {
         if(!gpio_get_level(BTN)) {
-            xTaskCreate(&task1, "task1", 2048, NULL, 5, NULL);
+            ESP_LOGI(TAG, "button press");
+            xTaskCreate(&task1, "task1", 2048, NULL, 1, NULL);
+            // soft bouncing bypass
             vTaskDelay(200 / portTICK_PERIOD_MS);
         }
+
+        // to avoi empty loop
+        vTaskDelay(20/portTICK_PERIOD_MS);
     }
 }
